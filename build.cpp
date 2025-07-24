4a19c6b27007659359069b81aa766730fece4fa7
JPhillip Kohberger <jphillipkohberger@yahoo.com>
2025-07-24 13:59:01 -0400

    Refactor build code

--- build.cpp
+++ build.cpp
@@ -5,10 +5,14 @@
 #include <iostream>
 
 //./server many pool &>/dev/null &
-//curl --http0.9 -d "foo=bar&bar=foo" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://localhost:8080/test/html/index.html
-//pkill server && g++ -std=c++17 main.cpp -o server && ./server many pooled &
-//curl --http0.9 -d "foo=bar&bar=foo" -H "Content-Type: application/x-www-form-urlencoded" -X POST http://127.0.0.1:8080/test/html/index.html
-// curl --http0.9 -X POST http://127.0.0.1:8080/test/html/index.html -H 'Content-Type: application/json' -d '{"name":"Leo","age":26}'
+// curl --http0.9 -d "foo=bar&bar=foo" -H "Content-Type:
+// application/x-www-form-urlencoded" -X POST
+// http://localhost:8080/test/html/index.html pkill server && g++ -std=c++17
+// main.cpp -o server && ./server many pooled & curl --http0.9 -d
+// "foo=bar&bar=foo" -H "Content-Type: application/x-www-form-urlencoded" -X
+// POST http://127.0.0.1:8080/test/html/index.html
+// curl --http0.9 -X POST http://127.0.0.1:8080/test/html/index.html -H
+// 'Content-Type: application/json' -d '{"name":"Leo","age":26}'
 /***
  * CLI - Need functions for compiling various files, executing
  * various files, automatic compilation on file save
@@ -17,60 +21,40 @@
 class CLI {
 
 public:
+  char **args;
+  int arg_count;
+  void set_args(char *args[]) {
+    std::cout << "Number of arguments (argc): " << this->arg_count << std::endl;
+    ;
+    for (int i = 0; i < this->arg_count; ++i) {
+      std::cout << "Argument " << i << ": " << this->args[i] << std::endl;
+    }
+  }
 
-	char** args;
-	int arg_count;
-	
-	CLI(char** args) {
-	
-		this->arg_count = sizeof(args) - 1;
-		this->args = args;
-		
-		std::cout << "Number of arguments (argc): " << this->arg_count;
-		std::cout << std::endl;
-    	std::cout << "Program name (argv[0]): " << this->args[0] << std::endl;
-
-    	if (this->arg_count > 1) {
-       		std::cout << "Additional arguments:" << std::endl;
-        	for (int i = 1; i < this->arg_count; ++i) {
-           	std::cout << "args[" << i << "]: " << this->args[i] << std::endl;
-        	}
-    	}
-	
-	}
-	
-	void build() {
-		
-		std::cout << "Building project 2:" << std::endl;
-		
-		int returnCode = system("g++ -std=c++17 main.cpp -o server");
-		
-		if (returnCode == 0) {
-       		
-			std::cout << "Built successfully" << std::endl;
-    	
-		} else {
-       		
-			std::cout << "Build failed: " << returnCode << std::endl;
-    	
-		}
-		
-	}
-
+  void build() {
+    int returnCode = system("g++ -std=c++17 main.cpp -o server");
+    // int returnCode = 3;//system("echo hey there");
+    if (returnCode == 0) {
+      std::cout << "Built successfully" << std::endl;
+    } else {
+      std::cout << "Build failed: " << returnCode << std::endl;
+    }
+  }
 };
 
 // g++ -std=c++17 main.cpp -o server && echo "completed"
 // g++ -std=c++17 build.cpp -o build && echo "completed"
-// wget --post-data "user=fo&word=bar" http://127.00.0.1:8080/test/html/index.html &> /dev/null
-// wget http://127.00.0.1:8080/test/html/index.html &> /dev/null
-int main(int argc, char* argv[]) {
-
-	CLI* cli = new CLI(argv);
-	
-	cli->build();
-	
-    return 0;
-
+// wget --post-data "user=fo&word=bar"
+// http://127.00.0.1:8080/test/html/index.html &> /dev/null wget
+// http://127.00.0.1:8080/test/html/index.html &> /dev/null
+int main(int argc, char *argv[]) {
+  CLI *cli = new CLI();
+  cli->args = argv;
+  cli->arg_count = argc;
+  cli->set_args(argv);
+  cli->build();
+  delete cli;
+  return 0;
 }
 
 #endif
